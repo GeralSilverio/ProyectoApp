09b8a94df3ddeba59e0bea572202e9d9a95a1bc2
Gerald Silverio <geraldsilverio412@gmail.com>
2023-03-01 20:28:11 -0400

    Primera Fase-Menu

--- Aplicacion APP SCHOOL/Aplicacion APP SCHOOL/Form1.cs
+++ Aplicacion APP SCHOOL/Aplicacion APP SCHOOL/Form1.cs
@@ -16,5 +16,15 @@ namespace Aplicacion_APP_SCHOOL
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        //-----------------------------Boton Salir--------------------------------------------------
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
